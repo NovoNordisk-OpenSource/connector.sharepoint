610b053da7f1ac9d363f0ce25ca2b348c3c8437c
CGID (Cervan Girard) <CGID@novonordisk.com>
2024-07-12 14:30:43 +0200

    Implement Unit tests (#5)
    * feat: Implement Unit tests

---
+++ tests/testthat/test-token.R
@@ -0,0 +1,41 @@
+test_that("Test set up of Token", {
+  skip_on_ci()
+  hash <- names(list_azure_tokens())[1]
+
+  ## Use the var env
+  withr::with_envvar(c("SHAREPOINT_AZURE_HASH" = hash),{
+    messages <- capture_messages(get_token())
+    expect_false(
+      any(grepl(x = messages, pattern = "No SHAREPOINT_AZURE_HASH env found"))
+    )
+  })
+
+  ## Error if no token
+  withr::with_envvar(c("R_AZURE_DATA_DIR" = "~"),{
+    quiet_token <- function(...){
+      suppressMessages(get_token(...))
+    }
+
+    expect_error(quiet_token())
+  })
+
+  ## Manipulate .active_file
+  file.rename(from = get_tk_active_file(), to = file.path(AzureAuth::AzureR_dir(), ".active_hash_old"))
+  messages_a <- capture_messages(get_token())
+
+  expect_true(
+    any(grepl(x = messages_a, pattern = "No hash provided or found, using the first token found"))
+  )
+
+  ## create one without SHAREPOINT
+  file.create(file.path(AzureAuth::AzureR_dir(), ".active_hash"))
+  messages_s <- capture_messages(get_token())
+  expect_true(
+    any(grepl(x = messages_s, pattern = "SHAREPOINT key not found in the active hash file"))
+  )
+  file.remove(file.path(AzureAuth::AzureR_dir(), ".active_hash"))
+
+  ## Restore .active_file
+  file.rename(from = file.path(AzureAuth::AzureR_dir(), ".active_hash_old"), to = file.path(AzureAuth::AzureR_dir(), ".active_hash"))
+
+})
